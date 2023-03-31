b81c8f69b53facc23b0eaca0f3fd542d9529cfa9
Lê Minh Hào <haotp25@gmail.com>
2023-03-31 23:17:35 +0700

    Hoàn thành được chức năng: tạo lớp học, có thể following và Going lớp học ,xem danh sách lớp học Going, xem danh sách lớp học của mình tạo ra.

---
+++ Migrations/202303301350009_AddIsCanceledColumnToCourse.cs
@@ -0,0 +1,20 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsCanceledColumnToCourse : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "IsCanceled", c => c.Boolean(nullable: false));
+            DropColumn("dbo.Courses", "IsDeleted");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Courses", "IsDeleted", c => c.Boolean(nullable: false));
+            DropColumn("dbo.Courses", "IsCanceled");
+        }
+    }
+}
--- ViewModels/CourseViewModel.cs
+++ ViewModels/CourseViewModel.cs
@@ -10,6 +10,7 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        public int Id { get; set; }
         [Required]
         public string Place { get; set; }
         [Required]
@@ -21,10 +22,14 @@ namespace BigSchool.ViewModels
         [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
+        public string Heading { get; set; }
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
         public DateTime GetDateTime()
         {
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            //DateTime date = DateTime.ParseExact("01-01-2022", "MM/dd/yyyy", CultureInfo.InvariantCulture);
         }
     }
 }
